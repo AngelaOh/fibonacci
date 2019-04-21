3c04d3c5cec3b6ed8764455d655280bf10783db3
Angela <angela.ohh@gmail.com>
2019-04-21 15:09:13 -0700

    recursive solution

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -7,6 +7,49 @@
 
 # Time complexity: ?
 # Space complexity: ?
+def fibonacci_non_recursion(n)
+  # n i step nth fibonacci number in series
+  # if n = 5
+  # 0 1 1 2 3
+  # set first_num = 0
+  # set second_num = 1
+  # set count of number to be 3
+  # until the count is greater than the input n
+  # new number is equal to first_num + second_num
+  # reassign first_num and second_num
+
+  if n.class != Integer || n < 0
+    raise ArgumentError, "Your input #{n} is not an non-negative integer!"
+  end
+
+  first_num = 0
+  second_num = 1
+  fibonacci_num = 0
+  count = 2
+
+  if n == 0
+    return fibonacci_num
+  elsif n == 1
+    return fibonacci_num + 1
+  else
+    until count > n
+      fibonacci_num = first_num + second_num
+      first_num = second_num
+      second_num = fibonacci_num
+      count += 1
+    end
+  end
+  return fibonacci_num
+end
+
 def fibonacci(n)
-  raise NotImplementedError
+  if n.class != Integer || n < 0
+    raise ArgumentError, "Your input #{n} is not an non-negative integer!"
+  end
+
+  if n < 2
+    return n
+  else
+    return fibonacci(n - 1) + fibonacci(n - 2)
+  end
 end
